aae1e8606e6fd64dcb6d5bf83fe0bee6ac497043
limeunha <eh1501@naver.com>
2025-01-10 18:11:57 +0900

    up

--- diary-api/models/worry.js
+++ diary-api/models/worry.js
@@ -1,1 +1,32 @@
+const Sequelize = require('sequelize')
 
+module.exports = class Worry extends Sequelize.Model {
+   static init(sequelize) {
+      return super.init(
+         {
+            title: {
+               type: Sequelize.STRING(100),
+               allowNull: false,
+            },
+            content: {
+               type: Sequelize.TEXT,
+               allowNull: false,
+            },
+         },
+         {
+            sequelize,
+            timestamps: true,
+            underscored: false,
+            modelName: 'Worry',
+            tableName: 'Worries',
+            paranoid: true,
+            charset: 'utf8mb4',
+            collate: 'utf8mb4_general_ci',
+         }
+      )
+   }
+
+   static associate(db) {
+      db.Worry.belongsTo(db.User, { foreignKey: 'UserId' })
+   }
+}
